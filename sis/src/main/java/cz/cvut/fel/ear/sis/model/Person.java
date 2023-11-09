e791de490c2007ec2fe7155a40daa3763e785a88
Matyas Urban <urban.matyas@icloud.com>
2023-11-09 11:48:27 +0100

    project setup

---
+++ sis/src/main/java/cz/cvut/fel/ear/sis/model/Person.java
@@ -0,0 +1,87 @@
+package cz.cvut.fel.ear.sis.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+public class Person  {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private LocalDate birthDate;
+
+    public Person(String firstName, String lastName, String email, String phoneNumber, LocalDate birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
+    }
+
+    public Person() {
+
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Person(" + "ID:" + this.id + " ,BIRTHDATE:" + this.birthDate +
+                "\nNAME:" + this.firstName + " " + this.lastName +
+                "\nCONTACT:" + this.email + " " + this.phoneNumber + ")";
+    }
+}
