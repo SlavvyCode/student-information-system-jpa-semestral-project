47c1c8985c00d661602a849ab60f5adc8ab174dd
Matyas Urban <urban.matyas@icloud.com>
2023-11-09 11:52:53 +0100

    added

---
+++ sis/src/main/java/cz/cvut/fel/ear/sis/SisApplication.java
@@ -0,0 +1,30 @@
+package cz.cvut.fel.ear.sis;
+
+import cz.cvut.fel.ear.sis.dao.PersonRepository;
+import cz.cvut.fel.ear.sis.model.Person;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+
+@SpringBootApplication
+public class SisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SisApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(PersonRepository repository){
+        return (args -> {
+            insertPeople(repository);
+            System.out.println(repository.findAll());
+        });
+    }
+    public void insertPeople(PersonRepository repository) {
+        repository.save(new Person("First", "Last", "m@m.cz", "1", LocalDate.now()));
+    }
+
+}
---
+++ sis/src/test/java/cz/cvut/fel/ear/sis/SisApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.ear.sis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
