7706539bcb93d7a7a60ff578bf9a0ff36e4338f5
Dardun <4dam.strobl@gmail.com>
2023-11-12 12:32:07 +0100

    swapped fall spring

--- sis/src/main/java/cz/cvut/fel/ear/sis/utils/enums/SemesterType.java
+++ sis/src/main/java/cz/cvut/fel/ear/sis/utils/enums/SemesterType.java
@@ -2,8 +2,8 @@ package cz.cvut.fel.ear.sis.utils.enums;
 import java.time.MonthDay;
 
 public enum SemesterType {
-    FALL(MonthDay.of(1, 1), MonthDay.of(6, 30)),
-    SPRING(MonthDay.of(7, 1), MonthDay.of(12, 31));
+    SPRING(MonthDay.of(1, 1), MonthDay.of(6, 30)),
+    FALL(MonthDay.of(7, 1), MonthDay.of(12, 31));
 
     private final MonthDay startDate;
     private final MonthDay endDate;
